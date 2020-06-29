314b482eb30a82949be5c3515265ca3a3346c0ab
qiulujin@myweplus.com <qiulujin@myweplus.com>
2020-06-29 10:10:49 +0800

    init

---
+++ src/main/java/com/wj/updater/ClientUpdaterApplication.java
@@ -0,0 +1,16 @@
+package com.wj.updater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class ClientUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientUpdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wj/updater/IntallUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wj.updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntallUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
